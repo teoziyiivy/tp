8d8b2d3410a9656bff00df5c3893e0492727d634
EdwardZYWang <wangzy.edward@u.nus.edu>
2021-10-07 15:53:36 +0800

    gradle edits

--- src/main/java/seedu/duke/CommandManager.java
+++ src/main/java/seedu/duke/CommandManager.java
@@ -1,10 +1,10 @@
 package seedu.duke;
-import seedu.duke.gym.GymManager;
 
+import seedu.duke.gym.GymManager;
 import java.util.Scanner;
 
 public class CommandManager {
-    GymManager GM = new GymManager();
+    GymManager gymManager = new GymManager();
 
     public void commandChecker() {
         String input;
@@ -21,11 +21,11 @@ public class CommandManager {
 
             } else if (command.equals(Keywords.INPUT_WORKOUT)) {
                 //input = workoutInputCommand(input, in);
-                GM.doneGymWorkout(result[1]);
+                gymManager.doneGymWorkout(result[1]);
 
             } else if (command.equals(Keywords.INPUT_WORKOUT_SCHEDULE)) {
                 //input = workoutInputCommand(input, in);
-                GM.addGymWorkout(result[1]);
+                gymManager.addGymWorkout(result[1]);
 
             } else if (command.equals(Keywords.INPUT_DRINKS)) {
                 //input = drinksInputCommand(input, in);
@@ -47,25 +47,5 @@ public class CommandManager {
         System.out.println("    Bye! Hope to see you again soon!");
 
     }
-
-//    private static String mealInputCommand(String input, Scanner in){
-//        Task whatMeal = TaskList.getMealMethod(input);
-//        System.out.println("Noted! CLI.ckFit has recorded your meal of" +
-//                [calories kcal] + "has been added to the calorie count!");
-//        return input;
-//    }
-//    private static String drinksInputCommand(String input, Scanner in){
-//        return input;
-//    }
-//    private static String workoutInputCommand(String input, Scanner in){
-//        return input;
-//    }
-//    private static String addweightInputCommand(String input, Scanner in){
-//        return input;
-//    }
-//    private static String checkweightInputCommand(String input, Scanner in){
-//        return input;
-//    }
-
 
 }
