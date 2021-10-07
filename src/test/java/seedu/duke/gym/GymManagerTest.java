c18c2355699b98fe476a67e97248e841995ee9a1
arvejw <jiewen.tehh@gmail.com>
2021-10-07 22:11:27 +0800

    Edit for checkstyle

--- src/test/java/seedu/duke/gym/GymManagerTest.java
+++ src/test/java/seedu/duke/gym/GymManagerTest.java
@@ -2,13 +2,13 @@ package seedu.duke.gym;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GymManagerTest {
 
     @Test
     // test for exception thrown in event of non-integer input for calories
-    void doneGymWorkout_non_integer_input(){
+    void doneGymWorkout_non_integer_input() {
         GymManager gm = new GymManager();
         String input = "chest day /at 23:59 /c $@$!";
         assertThrows(NumberFormatException.class, () -> gm.doneGymWorkout(input));
