35805f0b8cc5690d7f9d6a027803da892dd761de
arvejw <jiewen.tehh@gmail.com>
2021-10-09 03:08:16 +0800

    Tweak CommandManager to handle single word inputs correctly

--- src/main/java/seedu/duke/CommandManager.java
+++ src/main/java/seedu/duke/CommandManager.java
@@ -21,9 +21,13 @@ public class CommandManager {
         String input;
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
-        String[] inputArguments = input.trim().split(" ", 2);
-        String command = inputArguments[0];
-
+        String[] splitResult = input.trim().split(" ", 2); //todo: maybe also add this to parser class?
+        String command = splitResult[0];
+        // naming/way is implemented can be changed,
+        // to handle input= oneword e.g "addschedule" which does not split so inputArguments.length will be 1
+        // basically this is to avoid accessing inputArguments[1] which is out of bounds
+        // inputArguments is null if user input < 2 words
+        String inputArguments = (splitResult.length == 2) ? splitResult[1] : null;
 
         while (!(command.equals(Keywords.INPUT_BYE))) {
 
@@ -31,10 +35,10 @@ public class CommandManager {
                 meal.addMeal(input);
 
             } else if (command.equals(Keywords.INPUT_WORKOUT)) {
-                gymManager.addCompletedWorkout(inputArguments[1]);
+                gymManager.addCompletedWorkout(inputArguments);
 
             } else if (command.equals(Keywords.INPUT_WORKOUT_SCHEDULE)) {
-                gymManager.addScheduledWorkout(inputArguments[1]);
+                gymManager.addScheduledWorkout(inputArguments);
 
             } else if (command.equals(Keywords.INPUT_DRINKS)) {
                 fluid.sayDrank(input);
@@ -49,9 +53,9 @@ public class CommandManager {
                 System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");//overall error check
             }
             input = in.nextLine();
-            inputArguments = input.trim().split(" ", 2);
-            command = inputArguments[0];
-
+            splitResult = input.trim().split(" ", 2);
+            command = splitResult[0];
+            inputArguments = (splitResult.length == 2) ? splitResult[1] : null;
         }
         System.out.println("    Bye! Hope to see you again soon!");
 
