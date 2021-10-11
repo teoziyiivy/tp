91402904bc77ab718db02c777aa2fe321b94f082
arvejw <jiewen.tehh@gmail.com>
2021-10-11 18:04:45 +0800

    Update JUnit

--- src/test/java/seedu/duke/MealTest.java
+++ src/test/java/seedu/duke/MealTest.java
@@ -3,13 +3,15 @@ package seedu.duke;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.format.DateTimeParseException;
+
 class MealTest {
 
     @Test
     void addMeal() {
         Meal meal = new Meal();
-        String input = "ate pasta 14/09/2021";
-        Assertions.assertThrows(NumberFormatException.class, () -> {
+        String input = "ate pasta /c 356 /d 14/09/2021 /t 7pm";
+        Assertions.assertThrows(DateTimeParseException.class, () -> {
             meal.addMeal(input);
         });
     }
