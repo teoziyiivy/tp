d1e4715e289da616eece722d5e645fbb0ac1907e
VishalJeyaram <vishalsonic2000@gmail.com>
2021-10-13 23:46:02 +0800

    Messages updated

--- src/main/java/seedu/duke/ClickfitMessages.java
+++ src/main/java/seedu/duke/ClickfitMessages.java
@@ -46,4 +46,15 @@ public class ClickfitMessages {
                     + System.lineSeparator() + "\n";
 
     public static final String HELP_UG = "Here is the link to our User Guide!";
+
+    public static final String DATE_ERROR = "Please enter your date in the right format. " +
+                                                 "It should be \"DD/MM/YYYY\"";
+
+    public static final String TIME_ERROR = "Please enter your time in the right format. " +
+                                                 "It should be \"HH:MM\"";
+
+    public static final String NAME_ERROR = "Please enter the name or description of your meal/fluid";
+
+    public static final String NUMBER_ERROR = "Please enter the calories/volume properly";
+
 }
