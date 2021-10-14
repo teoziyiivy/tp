894fbf340a1c29bb37ae7084067e8599c95ed96d
pragyan_01 <samal@u.nus.edu>
2021-10-14 18:44:18 +0800

    add indentation

--- src/test/java/seedu/duke/FluidTest.java
+++ src/test/java/seedu/duke/FluidTest.java
@@ -46,15 +46,4 @@ class FluidTest {
         String input = "deletefluid one";
         assertThrows(NumberFormatException.class, () -> fluid.deleteFluid(input));
     }
-
-//    @Test
-//    void listFluid() throws DukeException {
-//        Fluid fluid = new Fluid();
-//        ArrayList<String> fluidArray;
-//        fluidArray = new ArrayList<>();
-//        fluid.addFluid("coke /c 40 /v 100 /d 12/12/2021 /t 10:30");
-//        fluid.addFluid("water /c 0 /v 200 /d 08/10/2021 /t 10:30");
-//        //assertThrows(DateTimeParseException.class, fluid::listFluid);
-//        assert false : "Fluid array should not be empty";
-//    }
 }
