139536357217d962d84fe986f13bd83088a23380
arvejw <jiewen.tehh@gmail.com>
2021-10-31 21:59:52 +0800

    Edit some exception for more intuitive messages

--- src/main/java/seedu/duke/exceptions/workout/MissingWorkoutCalorieSeparatorException.java
+++ src/main/java/seedu/duke/exceptions/workout/MissingWorkoutCalorieSeparatorException.java
@@ -3,7 +3,8 @@ package seedu.duke.exceptions.workout;
 public class MissingWorkoutCalorieSeparatorException extends WorkoutException {
     @Override
     public String getMessage() {
-        return "Your workout is missing the calorie separator /c" + System.lineSeparator()
-                + "Please minimally have the format: add workout [workout_description] /c [calories]";
+        return "CLI.ckFit is having difficulties finding the calorie separator /c" + System.lineSeparator()
+                + "Please minimally have the format: add workout [workout_description] /c [calories]"
+                + System.lineSeparator() + "Do remember to put spaces between your separators!";
     }
 }
