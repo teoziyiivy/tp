f868a74150f44ef6429e882e95babe3fa0b6d371
teoziyiivy <e0559117@u.nus.edu>
2021-11-03 01:19:34 +0800

    Some JavaDoc + JUnits

--- src/test/java/seedu/duke/WeightTrackerTest.java
+++ src/test/java/seedu/duke/WeightTrackerTest.java
@@ -6,27 +6,83 @@ import seedu.duke.exceptions.weight.WeightException;
 import seedu.duke.exceptions.weight.DeleteWeightException;
 import seedu.duke.exceptions.weight.DeleteWeightIndexException;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 //@@author teoziyiivy
 public class WeightTrackerTest {
 
     @Test
-    void addWeight() {
+    void addWeight_validFormat1_noExceptionThrow() {
+        WeightTracker weights = new WeightTracker();
+        String input = "50 /d 03/11/2021";
+        assertDoesNotThrow(() -> weights.addWeight(input));
+    }
+
+    @Test
+    void addWeight_validFormat2_noExceptionThrow() {
+        WeightTracker weights = new WeightTracker();
+        String input = "50";
+        assertDoesNotThrow(() -> weights.addWeight(input));
+    }
+
+    @Test
+    void addWeight_invalidFormat_exceptionThrow() {
         WeightTracker weights = new WeightTracker();
         String input = "";
         Assertions.assertThrows(WeightException.class, () -> weights.addWeight(input));
     }
 
     @Test
-    void deleteWeight() {
+    void deleteWeight_validFormat_noExceptionThrow() throws WeightException {
+        WeightTracker weights = new WeightTracker();
+        String entry = "50 /d 03/11/2021";
+        String input = "1";
+        weights.addWeight(entry);
+        assertDoesNotThrow(() -> weights.deleteWeight(input));
+    }
+
+    @Test
+    void deleteWeight_invalidFormat_exceptionThrow() {
         WeightTracker weights = new WeightTracker();
         String input = "delete weight";
         Assertions.assertThrows(DeleteWeightException.class, () -> weights.deleteWeight(input));
     }
 
     @Test
-    void deleteWeightIndex() {
+    void deleteWeightIndex_invalidIndex_exceptionThrow() {
         WeightTracker weights = new WeightTracker();
         String input = "1";
         Assertions.assertThrows(DeleteWeightIndexException.class, () -> weights.deleteWeight(input));
+    }
+
+    @Test
+    void listWeightIndex_validFormat1_noExceptionThrow() throws WeightException {
+        WeightTracker weights = new WeightTracker();
+        String entry = "50 /d 03/11/2021";
+        weights.addWeight(entry);
+        String input = "03/11/2021";
+        assertDoesNotThrow(() -> weights.listWeights(input));
+    }
+
+    @Test
+    void listWeightIndex_validFormat2_noExceptionThrow() throws WeightException {
+        WeightTracker weights = new WeightTracker();
+        String entry1 = "50 /d 02/11/2021";
+        String entry2 = "45 /d 03/11/2021";
+        weights.addWeight(entry1);
+        weights.addWeight(entry2);
+        String input = "03/11/2021";
+        assertDoesNotThrow(() -> weights.listWeights(input));
+    }
+
+    @Test
+    void listWeightIndex_validFormat3_noExceptionThrow() throws WeightException {
+        WeightTracker weights = new WeightTracker();
+        String entry1 = "50 /d 02/11/2021";
+        String entry2 = "45 /d 03/11/2021";
+        weights.addWeight(entry1);
+        weights.addWeight(entry2);
+        String input = "all";
+        assertDoesNotThrow(() -> weights.listWeights(input));
     }
 }
