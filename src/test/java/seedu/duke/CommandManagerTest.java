c2b3c62a8a6c489786aae33740788b37980beda5
EdwardZYWang <wangzy.edward@u.nus.edu>
2021-11-06 04:23:11 +0800

    add junits

--- src/test/java/seedu/duke/CommandManagerTest.java
+++ src/test/java/seedu/duke/CommandManagerTest.java
@@ -1,18 +1,10 @@
 package seedu.duke;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //@@author EdwardZYWang
 class CommandManagerTest {
-    /*
-    @Test
-    void commandManager() {
-        String input = "drank cola 300 12/12/2021";
-        String[] result = input.trim().split(" ", 2);
-        String command = result[0];
-        assert command.equals(Keywords.INPUT_DRINKS);
-    }
-     */
 
     @Test
     void commandManager1() {
@@ -21,16 +13,6 @@ class CommandManagerTest {
         String command = result[0];
         assert command.equals(Keywords.INPUT_ADD_WORKOUT);
     }
-
-    /*
-    @Test
-    void commandManager2() {
-        String input = "ate risotto and noodle 200 12/12/2022";
-        String[] result = input.trim().split(" ", 2);
-        String command = result[0];
-        assert command.equals(Keywords.INPUT_MEAL);
-    }
-     */
 
     @Test
     void commandManager3() {
@@ -71,4 +53,72 @@ class CommandManagerTest {
         String command = result[0];
         assert command.equals(Keywords.LIST_DRINKS);
     }
+
+    @Test
+    void foodBankParser1() {
+        String command = "";
+        String inputArguments = "addfluid coke";
+        String[] splitResults = inputArguments.trim().split(" ", 2);
+        command = splitResults[0];
+        inputArguments = (splitResults.length == 2) ? splitResults[1] : null;
+        assert command.equals(Keywords.ADD_FLUID);
+        assert inputArguments.equals("coke");
+    }
+
+    @Test
+    void foodBankParser2() {
+        String command = "";
+        String inputArguments = "addfluid";
+        String[] splitResults = inputArguments.trim().split(" ", 2);
+        command = splitResults[0];
+        inputArguments = (splitResults.length == 2) ? splitResults[1] : null;
+        assert command.equals(Keywords.ADD_FLUID);
+        assert inputArguments == null;
+    }
+
+    @Test
+    void listParser() {
+        String command = "";
+        String date = "";
+        String inputArguments = "meals 06/11/2022";
+        String[] splitResults = inputArguments.trim().split(" ", 2);
+        command = splitResults[0];
+        date = splitResults[1];
+        assert command.equals(Keywords.MEALS);
+        assert date.equals("06/11/2022");
+    }
+
+    @Test
+    void listParser1() {
+        String command = "";
+        String date = "";
+        String inputArguments = "meals";
+        String[] splitResults = inputArguments.trim().split(" ", 2);
+        command = splitResults[0];
+        assert command.equals(Keywords.MEALS);
+    }
+
+    @Test
+    void addParser1() {
+        String command = "";
+        String inputArguments = "";
+        String input = "meals pasta /c 100 /d 06/11/2021 /t 23:59";
+        String[] splitResults = input.trim().split(" ", 2);
+        command = splitResults[0];
+        inputArguments = (splitResults.length == 2) ? splitResults[1] : null;
+        assert command.equals(Keywords.MEALS);
+    }
+
+    @Test
+    void addParser2() {
+        String command = "";
+        String inputArguments = "";
+        String input = "meals";
+        String[] splitResults = input.trim().split(" ", 2);
+        command = splitResults[0];
+        inputArguments = (splitResults.length == 2) ? splitResults[1] : null;
+        assert command.equals(Keywords.MEALS);
+        assert inputArguments == null;
+    }
+
 }
